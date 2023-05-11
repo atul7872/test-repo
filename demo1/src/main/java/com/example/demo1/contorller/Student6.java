cb04d549b093a1337151da20ad29eb75a75b5a3b
atul <jagtapatulp@yahoo.co.in>
2023-05-11 16:56:47 -0400

    Added Student 6

---
+++ demo1/src/main/java/com/example/demo1/contorller/Student6.java
@@ -0,0 +1,25 @@
+package com.example.demo1.contorller;
+
+public class Student6 {
+	public int getNameid() {
+		return nameid;
+	}
+	public void setNameid(int nameid) {
+		this.nameid = nameid;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	private int nameid;
+	private String fname;
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	private String lname;
+}
