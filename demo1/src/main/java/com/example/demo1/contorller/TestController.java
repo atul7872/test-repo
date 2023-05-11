3c3717ed9e775435a52328eb1847b8a120e01e86
atul <jagtapatulp@yahoo.co.in>
2023-05-11 15:59:21 -0400

    Added all Classes and property file

---
+++ demo1/src/main/java/com/example/demo1/contorller/TestController.java
@@ -0,0 +1,16 @@
+package com.example.demo1.contorller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	@GetMapping("/")
+	Student  return1() {
+		Student st = new Student();
+		st.setFname("Atul");
+		st.setNameid(2);
+		System.out.println("Atul syso");
+		return st;
+	}
+}
